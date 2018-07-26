6250be619bc0797e1a9e60e7dbae837595fc5d3f
Doug Hoff <email@doughoff.com>
2018-07-26 12:19:06 -0500

    updates

---
+++ HelloWorld.js
@@ -0,0 +1,6 @@
+"use strict";
+{
+    var name_1 = "world";
+    console.log("Hello, " + name_1);
+    console.log("Hello, " + name_1);
+}
---
+++ HelloWorld.ts
@@ -0,0 +1,5 @@
+{
+  let name: string = "world";
+  console.log("Hello, " + name);
+  console.log("Hello, " + name);
+}
